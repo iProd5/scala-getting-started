a13e7c205668d187b66c0a68b267b6560309b2b6
iProd5 <franklinluther3@gmail.com>
2019-11-25 13:38:43 +0000

    finall

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -12,6 +12,7 @@ object MyApp extends App {
 
 //    printing three times a word
     for (threeTimes <- 1 to 3) print("hello")
+    println()
 
 //    even
 def even_?(num: Int) = num%2 ==0
@@ -21,14 +22,28 @@ def even_?(num: Int) = num%2 ==0
 def even(num1: Int) =if (num1%2 ==0) true else false
     even(3)
 
+    //odd
+//    def odd(x: Int)=!even(x)
 
-//    odd
-//def odd_?(num2: Int) = num2%2 ==0
-//    odd_?(5)
 
     def wordsToLowerCase(words: String*) = words map (_.toLowerCase)
     wordsToLowerCase("A","G")
 
-    def even_?(num: Int) = num%2 ==0
-    even_?(5)
+    def even2_?(num4: Int) = num4%2 ==0
+    even2_?(5)
+
+//    weekends
+def isWeekendDays(day: String) = day match{
+    case "saturday" | "sunday" => true
+    case _ => false
+}
+//    9
+    def areWeekends(days: String*) = days map(d=>isWeekendDays(d))
+
+//    foreach
+    areWeekends("monday", "saturday") foreach(println)
+
+//11
+    def stringNum(word: String, number: Int) = word * number
+    print(stringNum("Come", 5))
 }
